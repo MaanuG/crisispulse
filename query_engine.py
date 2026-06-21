0f09aa17206579ed201b4925267ace096c5302ba
Maanika Khanna <143287646+MaanuG@users.noreply.github.com>
2026-06-21 01:23:39 -0700

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -1,12 +1,8 @@
 from typing import List
 import spacy
-from spacy.cli import download
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
+
 
 
 # =========================================================
